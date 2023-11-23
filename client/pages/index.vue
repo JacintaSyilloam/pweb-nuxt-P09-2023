e2f2abddb5562d99c2f1e8585804de2016ee1e71
Samuel Yuma K <samuelyuma.117@gmail.com>
2023-11-23 11:09:33 +0700

    feat: init tailwindcss, modify CRUD of Logs collection

---
+++ client/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <main>
+        <div class="text-center flex flex-col gap-1 items-center justify-center h-screen">
+            <h1 class="text-sky-500 font-bold text-3xl">Hello World!</h1>
+            <p class="text-orange-500 text-lg underline">TailwindCSS Init Successfully!</p>
+        </div>
+    </main>
+</template>
--- server/src/collections/Log.js
+++ server/src/collections/Log.js
@@ -1,37 +1,32 @@
 /** @type {import('payload/types').CollectionConfig} */
 
 const Log = {
-  slug: "log",
-  access: {
-    create: () => true,
-    read: () => true,
-    update: () => true,
-    delete: () => true,
-  },
-  fields: [
-    {
-      name: "type",
-      label: "Type",
-      type: "text",
-      required: true,
+    slug: 'log',
+    access: {
+        create: () => false,
+        read: () => true,
+        update: () => false,
+        delete: () => true,
     },
-    {
-      name: "name",
-      label: "Title",
-      type: "text",
-      required: true,
-    },
-    {
-      name: "action",
-      type: "text",
-      required: true,
-    },
-    {
-      name: "timestamp",
-      type: "date",
-      required: true,
-    },
-  ],
+    fields: [
+        {
+            name: 'type',
+            label: 'Type',
+            type: 'text',
+            required: true,
+        },
+        {
+            name: 'name',
+            label: 'Title',
+            type: 'text',
+            required: true,
+        },
+        {
+            name: 'action',
+            type: 'text',
+            required: true,
+        },
+    ],
 };
 
 export default Log;
